fe0584c04184e50f8bf3d89ae02ca181f93fdd7f
max-student@outlook.com <max-student@outlook.com>
2019-08-06 11:56:01 -0400

    Add project files.

---
+++ EntityFrameworkBegins/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace EntityFrameworkBegins {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
