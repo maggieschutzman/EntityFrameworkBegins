3e622d5eb5a5367855e559de0a615920ebd71665
max-student@outlook.com <max-student@outlook.com>
2019-08-06 16:22:29 -0400

    Added linq examples

--- EntityFrameworkBegins/Program.cs
+++ EntityFrameworkBegins/Program.cs
@@ -8,10 +8,22 @@ namespace EntityFrameworkBegins {
 
             var context = new AppDbContext();
 
-            var students = context.students.ToArray();
-            foreach (var student in students) {
-                Console.WriteLine($"{student.Firstname} {student.Lastname}");
-            }
+            //var students = context.students                
+            //    .Where(s => s.GPA >= 3.0)
+            //    .OrderByDescending(s => s.Lastname)
+            //    .ToArray();
+
+            //var students = context.students
+            //    .OrderBy(s => s.Firstname)
+            //    .ToArray();
+
+            var studentAverageSat = context.students
+                .Average(s => s.SAT);
+            Console.WriteLine($"The average sat score is {studentAverageSat}");
+
+            //foreach (var student in students) {
+            //    Console.WriteLine($"{student.Firstname} {student.Lastname}");
+            //}
         }
     }
 }
